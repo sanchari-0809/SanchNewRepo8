2431f6b5474dd8f906e963556e84d22618aa3832
sanchari-0809 <114421042+sanchari-0809@users.noreply.github.com>
2025-04-27 16:30:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello from your EC2 server!');
+const express = require('express');  // Assuming you are using Express
+const app = express();
+
+const PORT = 3000; // 👈 here you set your port number
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
